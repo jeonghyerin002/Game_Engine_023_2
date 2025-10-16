b29253b6a2c87ead38ee6959c2b6b66dc54c1e61
jeonghyerin002 <jhr102446@gmail.com>
2025-10-16 15:08:50 +0900

    음 안하는ㄱ구나

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [Header("Game Dataa")]
+    public int currentStage = 1;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
